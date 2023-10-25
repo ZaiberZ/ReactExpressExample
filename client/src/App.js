7036ac995c00c37123f94c10706425fdda21c441
Adrian Mendoza <catorce_de_enero@hotmail.com>
2023-10-25 14:18:10 -0600

    Ejemplo completado

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [backEndData, setBackEndData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(res => res.json()).then(data => {
+      setBackEndData(data);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(typeof backEndData.users === 'undefined') ? (<p>Loading</p>) :
+        (<ul>
+          {backEndData.users.map((u, i) => (<li id={i}>{u}</li>))}
+        </ul>)}
     </div>
-  );
+  )
 }
 
 export default App;
